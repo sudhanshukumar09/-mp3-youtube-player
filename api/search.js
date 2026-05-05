8679afc00759ab54405e8e3d9e41a31fb5aca9e1
sudhanshukumar09 <sudhanshukumar0128@gmail.com>
2026-05-05 18:09:16 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -15,14 +15,14 @@ export default async function handler(req, res) {
     });
   }
 
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoCategoryId=10&maxResults=12&q=${encodeURIComponent(query)}&key=${API_KEY}`;
+  const url =
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoCategoryId=10&maxResults=15&q=${encodeURIComponent(query)}&key=${API_KEY}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
 
     return res.status(200).json(data);
-
   } catch (error) {
     return res.status(500).json({
       error: "Failed to fetch YouTube results"
