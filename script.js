043fa1b3784267a1fa2cf083923f8975394a9a2f
sudhanshukumar09 <sudhanshukumar0128@gmail.com>
2026-05-05 16:38:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
+const API_BASE = "/api/search";
+
+let currentVideo = "";
+
 async function searchSongs() {
   const query = document.getElementById("searchInput").value;
 
-  const res = await fetch(`/api/search?q=${query}`);
+  const res = await fetch(`${API_BASE}?q=${query}`);
   const data = await res.json();
 
   const results = document.getElementById("results");
   results.innerHTML = "";
 
-  data.forEach(song => {
+  data.items.forEach(item => {
+    const videoId = item.id.videoId;
+    const title = item.snippet.title;
+    const image = item.snippet.thumbnails.medium.url;
+
     const div = document.createElement("div");
     div.className = "card";
 
     div.innerHTML = `
-      <img src="${song.image}" width="100">
-      <p>${song.title}</p>
-      <button onclick="playSong('${song.audio}', '${song.image}', '${song.title}')">
-        Play
+      <img src="${image}" width="100">
+      <p>${title}</p>
+      <button onclick="loadSong('${videoId}', '${title}', '${image}')">
+        Select
       </button>
     `;
 
@@ -23,8 +31,18 @@ async function searchSongs() {
   });
 }
 
-function playSong(audio, image, title) {
-  document.getElementById("audio").src = audio;
+function loadSong(videoId, title, image) {
+  currentVideo = videoId;
+
   document.getElementById("cover").src = image;
   document.getElementById("title").innerText = title;
+}
+
+function playSong() {
+  document.getElementById("player").src =
+    `https://www.youtube.com/embed/${currentVideo}?autoplay=1`;
+}
+
+function stopSong() {
+  document.getElementById("player").src = "";
 }
